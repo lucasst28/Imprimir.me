6a73d27c2090dc0895957a30a3fccc98bdf66b9f
lucasst28 <lucasseverino1@hotmail.com>
2023-11-13 16:07:53 -0300

    Seu mensagem de commit aqui

---
+++ src/Components/PaginaCadastro.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:248754ae46fea7233e62dda122ad8c4489e9c79c64237099b5f31d5910914bf5
+size 8292
--- src/Components/Paginalogin.jsx
+++ src/Components/Paginalogin.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:684128ca6a75543d27fbb4bf4b6eea0e5903ceb1e4a69927462ba799b008c226
-size 10116
+oid sha256:2ab7600c1db4ef0035463976fcd822387003f1f97d81569301edb494bd4fda1b
+size 10236
