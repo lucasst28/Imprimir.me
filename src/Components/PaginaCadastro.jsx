2d167c16aab2eeb45c965dc94d592204c8e9d0a8
lucasst28 <lucasseverino1@hotmail.com>
2023-11-15 14:14:08 -0300

    PáginaCadastro

--- src/Components/PaginaCadastro.jsx
+++ src/Components/PaginaCadastro.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:409b5feb40a3cbe8c2a15fbf0130dc6c5c3fbb3671f70861af1049259dd2f4f2
-size 7876
+oid sha256:4dd00bcf324f2c076cf2982a4cba9b6604be4fa0002c081cb83d048ccdfd9097
+size 8366
